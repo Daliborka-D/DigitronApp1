6bcb3ef6526934cd5be1ea5caaad1270baa81b68
Daliborka-D <daliborka.ddimitrijevic@gmail.com>
2024-07-03 11:55:34 +0200

    Add project files.

---
+++ DigitronApp1/BusinessLayer/OsnovnaKlasa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitronApp1.BusinessLayer
+{
+    internal class OsnovnaKlasa
+    {
+        public int prviBroj { get; set; }
+        public int drugiBroj { get; set; }
+
+        public int rezultat { get; set;}
+
+        public OsnovnaKlasa() {  prviBroj = 0; drugiBroj = 0; rezultat = 0;}
+
+        public int Sabiranje(int prviBroj, int drugiBroj)
+        {
+            int rezultat = prviBroj + drugiBroj;
+            return rezultat;
+        }
+        public int Oduzimanje(int prviBroj, int drugiBroj)
+        {
+            int rezultat = prviBroj - drugiBroj;
+            return rezultat;
+        }
+        public int Mnozenje(int prviBroj, int drugiBroj)
+        {
+            int rezultat = prviBroj * drugiBroj;
+            return rezultat;
+        }
+        public double Deljenje(double prviBroj, double drugiBroj)
+        {
+            if (drugiBroj == 0) { Console.WriteLine("Nemoguce je deliti sa 0"); return 0;  }
+            else {
+                double rezultat4 = prviBroj / drugiBroj;
+                return rezultat4;
+            }        
+                
+        }
+
+
+
+    }
+}
---
+++ DigitronApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DigitronApp1.BusinessLayer;
+
+Console.WriteLine("Unesi prvi broj: ");
+int prviBroj = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Unesi drugi broj: ");
+int drugiBroj = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Unesi redni broj zeljene operacije: 1 Sabiranje 2 Oduzimanje 3 Mnozenje 4 Deljenje");
+int operacija = Convert.ToInt32(Console.ReadLine());
+
+
+OsnovnaKlasa osnovna = new OsnovnaKlasa();
+
+switch (operacija)
+{
+    case 1:
+        int rezultat1 = osnovna.Sabiranje(prviBroj, drugiBroj);
+        Console.WriteLine($"Rezultat sabiranja je: {rezultat1}");
+        break;
+
+        case 2:
+        int rezultat2 = osnovna.Oduzimanje(prviBroj, drugiBroj);
+        Console.WriteLine($"Rezultat oduzimanja je: {rezultat2}");
+        break;
+
+        case 3:
+        int rezultat3 = osnovna.Mnozenje(prviBroj, drugiBroj);
+        Console.WriteLine($"Rezultat mnozenja je: {rezultat3}");
+        break;
+        
+        case 4:
+        double rezultat4 = osnovna.Deljenje(prviBroj, drugiBroj);
+        Console.WriteLine($"Rezultat deljenja je: {rezultat4}");
+        break;
+
+}
+
+
